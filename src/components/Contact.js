c076594a67a63e3450ebbfd5b310882c31b8fb6b
Charlie DiFranco <charlie.difranco2@gmail.com>
2021-03-01 22:23:57 -0600

    updated spacing and contact us button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -29,7 +29,7 @@ const Contact = () => {
         <input onChange={handleFormChange} value={state.user_name} name="user_name" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner" placeholder="NAME"></input>
         <input onChange={handleFormChange} value={state.user_email} name="user_email" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner" placeholder="EMAIL"></input>
         <textarea onChange={handleFormChange} value={state.message} name="message" rows="4" placeholder="MESSAGE" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner"></textarea>
-        <button type="submit" className="px-2 py-2 my-2 bg-green-500 hover:bg-green-700 rounded-md focus:outline-none focus:ring text-white font-semibold shadow-md uppercase border">SUBMIT</button>
+        <button type="submit" className="px-2 py-2 my-2 bg-blueGray-400 hover:bg-blueGray-600 rounded-md focus:outline-none focus:ring text-white font-semibold shadow-md uppercase border">SUBMIT</button>
       </form>
     </div>
   )
