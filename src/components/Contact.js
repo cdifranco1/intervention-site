eb6f67df60150a96bf33bcb01c6019dec022bd43
Charlie DiFranco <charlie.difranco2@gmail.com>
2021-03-25 19:24:50 -0500

    add phone number to message us

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,8 @@ import { useState } from 'react';
 const INITIAL_STATE = {
   user_name: "",
   user_email: "",
-  message: ""
+  message: "",
+  phone: ""
 };
 
 const Contact = () => {
@@ -29,8 +30,9 @@ const Contact = () => {
     <div className="mt-3 md:w-1/2 p-4 bg-white">
       <h4 className="text-2xl font-semibold uppercase text-blueGray-2 text-center tracking-wider">Message Us</h4>
       <form className="flex flex-col" onSubmit={handleFormSubmit}>
-        <input onChange={handleFormChange} value={state.user_name} name="user_name" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner text-blueGray-1" placeholder="NAME"></input>
-        <input onChange={handleFormChange} value={state.user_email} name="user_email" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner text-blueGray-1" placeholder="EMAIL"></input>
+        <input onChange={handleFormChange} value={state.user_name} name="user_name" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner text-blueGray-1" placeholder="NAME" />
+        <input onChange={handleFormChange} value={state.user_email} name="user_email" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner text-blueGray-1" placeholder="EMAIL"/>
+        <input onChange={handleFormChange} value={state.phone} name="phone" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner text-blueGray-1" placeholder="PHONE NUMBER" />
         <textarea onChange={handleFormChange} value={state.message} name="message" rows="4" placeholder="MESSAGE" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner text-blueGray-1"></textarea>
         <button type="submit" className="px-2 py-2 my-2 bg-blueGray-400 hover:bg-blueGray-600 rounded-md focus:outline-none focus:ring text-white font-semibold shadow-md uppercase border">SUBMIT</button>
       </form>
