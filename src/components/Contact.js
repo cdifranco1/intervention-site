929ad4063fe38b481e450d8d017ca3a6f92cc583
Charlie DiFranco <charlie.difranco2@gmail.com>
2021-04-19 22:12:25 -0500

    require form fields for contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,7 +10,6 @@ const INITIAL_STATE = {
 
 const CONVERSION_ID = 'Kc5KCNum3_0BEOze6scB';
 
-
 const Contact = () => {
   const [ state, setState ] = useState(INITIAL_STATE);
 
@@ -24,9 +23,10 @@ const Contact = () => {
   const handleFormSubmit = (e) => {
     e.preventDefault();
     emailjs.send("service_195kjs9", "contact_form", state, "user_QmOpOMxPzbv3nPEhopp5T");
+    window.gtag_report_conversion(CONVERSION_ID)
     
     setState(INITIAL_STATE);
-    window.gtag_report_conversion(CONVERSION_ID)
+    return;
   }
 
 
@@ -34,10 +34,10 @@ const Contact = () => {
     <div className="mt-3 md:w-1/2 p-4 bg-white">
       <h4 className="text-2xl font-semibold uppercase text-blueGray-2 text-center tracking-wider">Message Us</h4>
       <form className="flex flex-col" onSubmit={handleFormSubmit}>
-        <input onChange={handleFormChange} value={state.user_name} name="user_name" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner text-blueGray-1" placeholder="NAME" />
-        <input onChange={handleFormChange} value={state.user_email} name="user_email" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner text-blueGray-1" placeholder="EMAIL"/>
-        <input onChange={handleFormChange} value={state.phone} name="phone" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner text-blueGray-1" placeholder="PHONE NUMBER" />
-        <textarea onChange={handleFormChange} value={state.message} name="message" rows="4" placeholder="MESSAGE" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner text-blueGray-1"></textarea>
+        <input required onChange={handleFormChange} value={state.user_name} name="user_name" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner text-blueGray-1" placeholder="NAME" />
+        <input required onChange={handleFormChange} value={state.user_email} name="user_email" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner text-blueGray-1" placeholder="EMAIL"/>
+        <input required onChange={handleFormChange} value={state.phone} name="phone" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner text-blueGray-1" placeholder="PHONE NUMBER" />
+        <textarea required onChange={handleFormChange} value={state.message} name="message" rows="4" placeholder="MESSAGE" className="font-semibold my-2 p-2 rounded-sm focus:outline-none focus:ring border border-blueGray-2 shadow-inner text-blueGray-1"></textarea>
         <button type="submit" className="px-2 py-2 my-2 bg-blueGray-400 hover:bg-blueGray-600 rounded-md focus:outline-none focus:ring text-white font-semibold shadow-md uppercase border">SUBMIT</button>
       </form>
     </div>
